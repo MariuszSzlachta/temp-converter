854da89a967c3ab17e4da060dac342247ab60e21
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-05-15 17:24:52 +0200

    Finished 3/3 stages

--- js/script.js
+++ js/script.js
@@ -1,16 +1,90 @@
-var button = document.querySelector(".button"),
-  output = document.querySelector(".temp-output"),
+var button1 = document.querySelector('.button-1'),
+  button2 = document.querySelector('.button-2'),
+  output = document.querySelector('.temp-output'),
+  info = document.querySelector('.info'),
+  temp1,
+  temp2,
   tempValue;
 
-button.addEventListener('click', function() {
-  tempValue = window.prompt('Enter temp in Fahrenheit');
-  console.log(Number(tempValue));
 
-  if (tempValue != "" && tempValue !== "null" && Number(tempValue)) {
-    output.innerHTML = tempValue + ' stopni Fahrenheita to ' + (tempValue * 1.8 + 32) + ' stopni Celsiusza';
-  } else {
-    output.innerHTML = 'Zapisz poprawnie temperaturę';
+var toFahrenheit = function (tempValue) {
+  return Math.round(tempValue * 1.8 + 32);
+};
+
+var toCelsius = function (tempValue) {
+  return Math.round((tempValue - 32) / 1.8);
+};
+
+//fun water statement
+function waterStatementCels(tempValue, currentCallback) {
+  if (currentCallback == 'toFahrenheit') {
+
+    if (tempValue > 0 && tempValue < 100) {
+      info.innerHTML = 'In this temperature water is a liquid';
+    } else if (tempValue > 100) {
+      info.innerHTML = 'In this temperature water is a gas';
+    } else {
+      info.innerHTML = 'In this temperature water is a frost';
+    }
+  } else if (currentCallback == 'toCelsius') {
+
+    if (tempValue > 32 && tempValue < 212) {
+      info.innerHTML = 'In this temperature water is a liquid';
+    } else if (tempValue > 212) {
+      info.innerHTML = 'In this temperature water is a gas';
+    } else {
+      info.innerHTML = 'In this temperature water is a frost';
+    }
   }
-});
+}
+//end
+// switch (tempValue) {
+//   case tempValue > 32 && tempValue < 212:
+//     info.innerHTML = 'In this temperature water is a liquid';
+//     break;
 
- 
+//   case tempValue > 212:
+//     info.innerHTML = 'In this temperature water is a gas';
+//     break;
+
+//   default:
+//     info.innerHTML = 'In this temperature water is a frost';
+// }
+
+// // Function to run in eventListeners
+
+// function runToCelsius() {
+//   validate(toCelsius);
+// }
+
+// function runToFahrenheit() {
+//   validate(toFahrenheit);
+// }
+// // end
+
+function validate(callback) {
+  var currentCallback = callback.name;
+  tempValue = window.prompt('Enter temp value');
+  if (callback == toCelsius) {
+    temp1 = ' Fahrenheit';
+    temp2 = ' Celsius';
+  } else {
+    temp2 = ' Fahrenheit';
+    temp1 = ' Celsius';
+  }
+  console.log(callback(tempValue));
+  if (tempValue != "" && tempValue != "null" && !isNaN(tempValue) && (typeof(tempValue) != 'object')){
+    output.innerHTML = tempValue + temp1 + ' is ' + callback(tempValue) + temp2;
+    waterStatementCels(tempValue, currentCallback);
+  } else {
+    output.innerHTML = 'Incorrect format, try again';
+    info.innerHTML = "";
+  }
+};
+
+button1.addEventListener('click', function () {
+  validate(toCelsius);
+});
+button2.addEventListener('click', function () {
+  validate(toFahrenheit);
+});
